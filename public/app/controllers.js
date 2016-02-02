201e3be930b60e306e06dfaf2e347747bd937bd2
s-d-c <sdchute@gmail.com>
2016-02-01 17:20:45 -0800

    started browse controller

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -1,5 +1,10 @@
 angular.module('BookCtrls', ['BookServices'])
 .controller('HomeCtrl', ['$scope', 'Book', function($scope, Book) {
+
+	angular.element(document).ready(function(){
+        $(document).foundation();
+    });
+
 	$scope.books = [];
 
 	Book.query(function success(data) {
@@ -10,4 +15,18 @@ angular.module('BookCtrls', ['BookServices'])
 	});
 
 }])
+.controller('BrowseCtrl', ['$scope', 'Book', function($scope, Book) {
+	$scope.categories = { fiction: [], business: [], nature: [], computers: [] };
+
+	Book.query(function success(data) {
+		$scope.books = data;
+	}, function error(data) {
+		console.log(data)
+	});
+
+	$scope.populateCategories = function(books) {
+		
+	}
+}])
+
 
