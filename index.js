f578d6f349873a2fd14c24251d45d7ee10de4c3b
s-d-c <sdchute@gmail.com>
2016-02-07 16:59:21 -0800

    heroku

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 var mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/bookstore');
+console.log('===== Connecting to DB ... =====', uri);
+mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost/bookstore');
 
 // app.use('/data/search', expressJWT({secret: secret}));
 // app.use('/data/books', expressJWT({secret: secret}));
